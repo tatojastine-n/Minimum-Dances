6c8f23fb9bef62532854eab3f8ade15bb7c90ead
tatojastine-n <jajanicoletato@gmail.com>
2025-10-31 15:29:02 +0800

    Minimum Dances.cs

---
+++ Minimum Dances.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<int> a)
+    {
+        int minDistance = int.MaxValue;
+        bool found = false;
+
+        for (int i = 0; i < a.Count; i++)
+        {
+            for (int j = i + 1; j < a.Count; j++)
+            {
+                if (a[i] == a[j])
+                {
+                    int distance = j - i;
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        return found ? minDistance : -1;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.minimumDistances(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
